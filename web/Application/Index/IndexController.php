d2f491b26449b53b7b4843ea68fddbe5021a21df
nhegedus <norbert.hegedus91@gmail.com>
2016-03-21 23:45:23 +0200

    index page prototype

--- web/Application/Index/IndexController.php
+++ web/Application/Index/IndexController.php
@@ -19,7 +19,15 @@ class IndexController extends \SportArea\Core\Controller
 
     public function index()
     {
-        $this->render->renderPage('index.html.php');
+
+        $template = array(
+            'title'      => 'Sport is everywhere',
+        );
+
+        $this->render->addCss(BASE_URL . '/Application/Index/assets/css/index.css');
+        $this->render->setData('template', $template);
+        $this->render->addBuffer($this->render->renderView(ROOT . '/Application/Index/views/index.html.php'));
+        $this->render->renderPage('general.html.php');
 
         return true;
     }
---
+++ web/templates/general.html.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8 no-js"> <![endif]-->
+<!--[if IE 9]> <html lang="en" class="ie9 no-js"> <![endif]-->
+<!--[if !IE]><!-->
+<html lang="en" class="no-js">
+<!--[if !IE]><!-->
+<head>
+    <meta charset="UTF-8" />
+    <title><?php echo $template['title']?> | SportArea</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta content="width=device-width, initial-scale=1" name="viewport" />
+    <meta content="" name="description" />
+    <meta content="" name="author" />
+    <link rel="shortcut icon" href="<?php echo BASE_URL ?>/favicon.ico" />
+
+    <!-- BEGIN GLOBAL MANDATORY STYLES -->
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css"/>
+    <link href="<?php echo BASE_URL ?>/assets/vendor/metronic3.3.0/global/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+    <link href="<?php echo BASE_URL ?>/assets/vendor/metronic3.3.0/global/plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css"/>
+    <link href="<?php echo BASE_URL ?>/assets/vendor/metronic3.3.0/global/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+    <link href="<?php echo BASE_URL ?>/assets/vendor/metronic3.3.0/global/plugins/uniform/css/uniform.default.css" rel="stylesheet" type="text/css"/>
+    <link href="<?php echo BASE_URL ?>/assets/vendor/metronic3.3.0/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css" rel="stylesheet" type="text/css"/>
+    <!-- END GLOBAL MANDATORY STYLES -->
+
+    <!-- BEGIN OVERWRITE THEME STYLE -->
+    <link href="<?php echo BASE_URL ?>/assets/css/custom.css?version=<?=\Application\Settings\SettingsModel::get('environment') == 'development' ? time() : VERSION?>" rel="stylesheet" type="text/css"/>
+    <link href="<?php echo BASE_URL ?>/assets/css/public.css?version=<?=\Application\Settings\SettingsModel::get('environment') == 'development' ? time() : VERSION?>" rel="stylesheet" type="text/css"/>
+    <?php echo $css; ?>
+    <!-- END OVERWRITE THEME STYLE -->
+
+    <script type="text/javascript">
+        var BASE_URL = '<?php echo BASE_URL?>';
+    </script>
+
+    <!-- START CONTROLLER JS -->
+    <?php echo $js ?>
+    <!-- END -->
+
+</head>
+<body>
+    <?php echo $body; ?>
+</body>
+</html>
