5d97aa73e545b4dbf8f8cea58009fbd8e7ee48bd
nhegedus <norbert.hegedus91@gmail.com>
2016-04-12 07:01:31 -0700

    home page

---
+++ web/Application/Index/views/index.html.php
@@ -0,0 +1,35 @@
+<div class="wrapper">
+    <div class="header">
+
+    </div>
+    <div class="container-fluid">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="col-md-6">
+                <div class="title">Sport Area</div>
+                <div class="moto">Sport is everywhere,<br> just search for it!</div>
+                <form class="form">
+                    <div class="form-group">
+                        <label>What</label>
+                        <input type="text" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label>When</label>
+                        <input type="text" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label>Where</label>
+                        <input type="text" class="form-control">
+                    </div>
+                    <hr>
+                    <div class="shoot-btn">
+                        <input type="submit" class="btn btn-success" value="Shoot">
+                    </div>
+                </form>
+            </div>
+            <div class="col-md-6">
+
+            </div>
+        </div>
+    </div>
+</div>
