dc987d5f1896748bf0e6bccb9e407fe26f34fde3
adialexpop <apop@win.intacct.com>
2016-04-12 12:21:59 +0300

    another Test +

--- web/config.db.php
+++ web/config.db.php
@@ -7,17 +7,17 @@ $dbDeployment = "local";
 switch ($dbDeployment) {
     case "production":
         define("DB_HOST","188.241.222.184");
-        define("DB_USERNAME","sportare_devel");
+        define("DB_USERNAME","sportareadb");
         define("DB_PASSWORD","");
-        define("DB_NAME","sportare_devel");
+        define("DB_NAME","sportareadb");
         define('DB_DRIVER', 'mysql');
         break;
 
-    case "dev":
-        define("DB_HOST","188.241.222.184");
-        define("DB_USERNAME","sportare_devel");
-        define("DB_PASSWORD","");
-        define("DB_NAME","sportare_devel");
+    case "stage":
+        define("DB_HOST","localhost");
+        define("DB_USERNAME","sportare");
+        define("DB_PASSWORD","n7y9Y28tnB");
+        define("DB_NAME","sportare_stage");
         define('DB_DRIVER', 'mysql');
         break;
 
@@ -25,7 +25,23 @@ switch ($dbDeployment) {
         define('DB_HOST', '127.0.0.1');
         define('DB_USERNAME', 'root');
         define('DB_PASSWORD', '');
-        define('DB_NAME', 'sportarea_devel');
+        define('DB_NAME', 'sportareadb');
+        define('DB_DRIVER', 'mysql');
+        break;
+
+    case "dev01":
+        define("DB_HOST","localhost");
+        define("DB_USERNAME","sportare");
+        define("DB_PASSWORD","n7y9Y28tnB");
+        define("DB_NAME","sportare_dev01");
+        define('DB_DRIVER', 'mysql');
+        break;
+
+    case "dev02":
+        define("DB_HOST","localhost");
+        define("DB_USERNAME","sportare");
+        define("DB_PASSWORD","n7y9Y28tnB");
+        define("DB_NAME","sportare_dev02");
         define('DB_DRIVER', 'mysql');
         break;
 
@@ -33,5 +49,4 @@ switch ($dbDeployment) {
         die('Invalid deployment specified.');
         break;
 }
-
 ?>
